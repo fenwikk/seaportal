5f2abca958e8d76142ef297b46520c4f099085df
Filip Mårtensson <fenwikk@protonmail.com>
2022-08-03 10:28:55 +0200

    Compact Header

--- src/lib/components/Header/Navigation.svelte
+++ src/lib/components/Header/Navigation.svelte
@@ -1,55 +1,47 @@
-<script lang="ts" context="module">
-	export const load: Load = ({ params }) => {
-		if (!params.seaportal.startsWith('portal')) {
-			return {
-				status: 404
-			};
-		}
-
-		return {};
-	};
-</script>
-
 <script lang="ts">
 	import { browser } from '$app/env';
 
 	import { goto } from '$app/navigation';
-	import type { Load } from '@sveltejs/kit';
+	import { page } from '$app/stores';
 	import currentIndex from './stores/currentIndex';
 	import navigationElements from './stores/navigationElements';
 
 	const elements = $navigationElements;
 
-	if (browser)
-		currentIndex.set(
-			$navigationElements.findIndex((value) => value.url == location?.pathname || '')
-		);
+	currentIndex.set(
+		$navigationElements.findIndex(
+			(value) =>
+				(value.url.startsWith('/') ? value.url.substring(1) : value.url) == $page.params.seaportal
+		)
+	);
 
 	let hoveredIndex: number = -1;
-	let first = true;
+	let firstHover = true;
+	let hasDoneFirstSelect = false;
 </script>
 
 <ul
 	class="navigation"
 	on:mouseleave={() => {
-		first = true;
+		firstHover = true;
 		hoveredIndex = -1;
 	}}
-	on:mouseenter={() => (first = true)}
+	on:mouseenter={() => (firstHover = true)}
 >
 	<div
 		class="hover-element"
-		style="left: {(elements[hoveredIndex]?.element?.offsetLeft || 0) - 12}px;
+		style="left: {(elements[hoveredIndex]?.element?.offsetLeft || 0) - 9}px;
 			width: {elements[hoveredIndex]?.element?.clientWidth
-			? (elements[hoveredIndex]?.element?.clientWidth || 0) + 24
+			? (elements[hoveredIndex]?.element?.clientWidth || 0) + 18
 			: 0}px;
 			height: {elements[hoveredIndex]?.element?.clientHeight || 0}px;
-			transition-duration: {first ? '0' : '200'}ms;"
+			transition-duration: {firstHover ? '0' : '200'}ms;"
 	/>
 	<div
 		class="select-element"
 		style="left: {elements[$currentIndex]?.element?.offsetLeft || 0}px;
-			width: {elements[$currentIndex]?.element?.clientWidth || 0}px;"
+			width: {elements[$currentIndex]?.element?.clientWidth || 0}px;
+			transition-duration: {hasDoneFirstSelect ? '200' : '0'}ms;"
 	/>
 
 	{#each elements as element, i}
@@ -60,10 +52,11 @@
 					hoveredIndex = i;
 				}}
 				on:mouseleave={() => {
-					if (first) first = false;
+					if (firstHover) firstHover = false;
 				}}
 				on:click={() => {
 					goto(element.url);
+					hasDoneFirstSelect = true;
 					currentIndex.set(i);
 				}}
 			>
