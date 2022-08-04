467bfa44b59016d079ac3b297ee32a25bed5676f
Filip Mårtensson <fenwikk@protonmail.com>
2022-08-04 09:18:42 +0200

    Bigger page title

--- src/lib/components/PageTitle/PageTitle.svelte
+++ src/lib/components/PageTitle/PageTitle.svelte
@@ -3,9 +3,9 @@
 	export let subTitle: string = '';
 </script>
 
-<div class="bg-gray-50 border-b">
-	<div class="container py-10">
-		<h1 class="text-3xl font-serif">{@html title}</h1>
+<div class="border-b">
+	<div class="container py-14">
+		<h1 class="text-4xl">{@html title}</h1>
 		<p>{@html subTitle}</p>
 	</div>
 </div>
